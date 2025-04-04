ac06ce2d92602c994c5ed6bd956cd136433824cf
Juliano Sill <sill.juliano@gmail.com>
2025-04-03 22:10:15 -0300

    homolog test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="antialiased h-screen flex items-center justify-center flex-col gap-8">
       <h1 className="text-6xl font-semibold">Parabéns!</h1>
-      <p className="text-xl">Você me ajudou a testar a pipeline de deploy</p>
+      <p className="text-2xl">Você me ajudou a testar a pipeline de deploy</p>
       <p>;)</p>
     </div>
   );
